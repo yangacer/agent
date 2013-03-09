876472ff741f73d0b74c10cf79d0606e8ad655f0
yangacer <yangacer@gmail.com>
2013-03-10 02:09:22 +0800

    Add dynamic_bitset (extended from boost's one).

---
+++ detail/dynamic_bitset.hpp
@@ -0,0 +1,79 @@
+#ifndef AGENT_DYNAMIC_BITSET_HPP_
+#define AGENT_DYNAMIC_BITSET_HPP_
+
+#define private protected
+#include <boost/dynamic_bitset.hpp>
+#undef private
+
+template <
+  typename Block = unsigned long, 
+  typename Alloc = std::allocator<Block> >
+class dynamic_bitset : public boost::dynamic_bitset<Block, Alloc>
+{
+  typedef boost::dynamic_bitset<Block, Alloc> super_;
+  using super_::num_blocks;
+  using super_::m_bits;
+  using super_::bits_per_block;
+  using super_::block_index;
+  using super_::bit_index;
+public:
+  using typename super_::size_type;
+  using typename super_::block_width_type;
+  
+  explicit
+  dynamic_bitset(const Alloc& alloc = Alloc())
+  : super_(alloc)
+  {}
+
+  explicit 
+  dynamic_bitset(size_type num_bits, unsigned long value = 0,
+                 const Alloc& alloc = Alloc())
+  : super_(num_bits, value, alloc)
+  {}
+
+  template <typename BlockInputIterator>
+  dynamic_bitset(BlockInputIterator first, BlockInputIterator last,
+                 const Alloc& alloc = Alloc())
+  : super_(first, last, alloc)
+  {}
+
+  dynamic_bitset(const dynamic_bitset& b)
+  : super_(b)
+  {}
+
+  size_type find_first(bool on_off = true) const
+  {
+    return on_off ? super_::find_first() : m_do_find_off_from(0);
+  }
+
+  size_type find_next(size_type pos, bool on_off = true) const
+  {
+    if(on_off) return super_::find_next(pos);
+    
+    const size_type sz = super_::size();
+    if( pos >= (sz-1) || 0 == sz)
+      return super_::npos;
+    
+    ++pos;
+    size_type const blk = block_index(pos);
+    block_width_type ind = bit_index(pos);
+    Block fore = ~m_bits[blk] & (~Block(0) << ind);
+
+    return fore ?
+      blk * bits_per_block + boost::lowest_bit(fore) :
+      m_do_find_off_from(blk + 1);
+  }
+private:
+  size_type m_do_find_off_from(size_type first_block) const
+  {
+    size_type i = first_block;
+
+    while( i < num_blocks() && m_bits[i] == ~Block(0))
+      ++i;
+    if(i >= num_blocks())
+      return super_::npos;
+    return i * bits_per_block + boost::lowest_bit(~m_bits[i]);
+  }
+};
+
+#endif
