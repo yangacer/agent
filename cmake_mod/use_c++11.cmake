79e32a3f1cf7331f2794529441fb822e33adfa0e
yangacer <yangacer@ARMVM185.(none)>
2013-03-15 14:13:45 +0800

    Fix use_c++11.

--- cmake_mod/use_c++11.cmake
+++ cmake_mod/use_c++11.cmake
@@ -2,12 +2,14 @@
 # Add proper flags for compiling code in C++11.
 # TODO Add more compiler detection.
 
+message ( "use_c++11 mod: ${CMAKE_CXX_COMPILER_ID}" ) 
+
 if(CMAKE_COMPILER_IS_GNUCXX)
   message ("GNUCXX(g++) is detected. Apply C++11 flags.")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x")
 elseif(${CMAKE_CXX_COMPILER} MATCHES "Visual Studio 10")
   message ("MSVC 10 is detected. C++11 is supported")
-elseif(${CMAKE_CXX_COMPILER} MATCHES "clang")
+elseif(${CMAKE_CXX_COMPILER_ID} MATCHES "Clang")
   message ("Clang is detected. Apply C++11 flags.")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x")
 else()
